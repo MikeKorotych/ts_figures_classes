e20f3ceec2c8e8c8e61a3fc7021d65280dbf9e62
Mike <mixakievchanen@gmail.com>
2025-08-06 23:08:49 +0300

    fixed2

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,8 +1,6 @@
 type Shape = 'triangle' | 'circle' | 'rectangle';
 type Color = 'red' | 'green' | 'blue';
 
-const validColors = ['red', 'green', 'blue'];
-
 export interface Figure {
   shape: Shape;
   color: Color;
@@ -10,87 +8,70 @@ export interface Figure {
 }
 
 export class Triangle implements Figure {
+  public readonly shape: Shape = 'triangle';
+
   constructor(
     public color: Color,
     public a: number,
     public b: number,
     public c: number,
-    public shape: Shape,
   ) {
-    this.a = a;
-    this.b = b;
-    this.c = c;
-    this.color = color;
-    this.shape = shape;
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('Side length must be greater than 0');
+    }
 
-    if (!validColors.includes(this.color)) {
-      throw new Error('Color must be one of: red, green, blue');
+    if (a + b <= c || a + c <= b || b + c <= a) {
+      throw new Error('The sum of any two sides must be > than the third side');
     }
   }
 
   getArea(): number {
-    if (this.a <= 0 || this.b <= 0 || this.c <= 0) {
-      throw new Error('Side length must be greater than 0');
-    }
+    const p = (this.a + this.b + this.c) / 2;
+    const area = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
 
-    const sides = [this.a, this.b, this.c];
-    const longestSide = Math.max(...sides);
-
-    sides.splice(sides.indexOf(longestSide), 1);
-
-    if (longestSide >= sides[0] + sides[1]) {
-      throw new Error('The sum of any two sides must be > than the third side');
-    }
-
-    const p = 0.5 * (this.a + this.b + this.c);
-    const result = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
-
-    return Math.floor(result * 100) / 100;
+    return Math.floor(area * 100) / 100;
   }
 }
 
 export class Circle implements Figure {
+  public readonly shape: Shape = 'circle';
+
   constructor(
     public color: Color,
     public radius: number,
-    public shape: Shape,
   ) {
-    this.color = color;
-    this.radius = radius;
-    this.shape = shape;
+    if (radius <= 0) {
+      throw new Error('Radius must be a positive number');
+    }
   }
 
   getArea(): number {
-    if (this.radius <= 0) {
-      throw new Error('Radius must be a positive number');
-    }
+    const area = Math.PI * this.radius ** 2;
 
-    return Math.floor(Math.PI * this.radius ** 2 * 100) / 100;
+    return Math.floor(area * 100) / 100;
   }
 }
 
 export class Rectangle implements Figure {
+  public readonly shape: Shape = 'rectangle';
+
   constructor(
     public color: Color,
-    public shape: Shape,
     public width: number,
     public height: number,
   ) {
-    this.color = color;
-    this.shape = shape;
-    this.width = width;
-    this.height = height;
+    if (width <= 0 || height <= 0) {
+      throw new Error('Rectangle width and height must be positive numbers.');
+    }
   }
 
   getArea(): number {
-    if (this.width <= 0 || this.height <= 0) {
-      throw new Error('Rectangle width and height must be > 0');
-    }
+    const area = this.width * this.height;
 
-    return Math.floor(this.width * this.height * 100) / 100;
+    return Math.floor(area * 100) / 100;
   }
 }
 
 export function getInfo(figure: Figure): string {
-  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
+  return `This is a ${figure.color} ${figure.shape} with area ${figure.getArea()}.`;
 }
