01085228affa9cd963d3956e211c942792cd1926
Mike <mixakievchanen@gmail.com>
2025-08-06 22:46:45 +0300

    solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,90 @@
-export interface Figure {}
+type Shape = 'triangle' | 'circle' | 'rectangle';
+type Color = 'red' | 'green' | 'blue';
 
-export class Triangle implements Figure {}
+export interface Figure {
+  shape: Shape;
+  color: Color;
+  getArea(): number;
+}
 
-export class Circle implements Figure {}
+export class Triangle implements Figure {
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+    public shape: Shape,
+  ) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    this.color = color;
+    this.shape = shape;
+  }
 
-export class Rectangle implements Figure {}
+  getArea(): number {
+    if (this.a <= 0 || this.b <= 0 || this.c <= 0) {
+      throw new Error('your error message');
+    }
 
-export function getInfo(figure): string {
-  return typeof figure;
+    const sides = [this.a, this.b, this.c];
+    const longestSide = Math.max(...sides);
+
+    sides.splice(sides.indexOf(longestSide), 1);
+
+    if (longestSide >= sides[0] + sides[1]) {
+      throw new Error('your error message');
+    }
+
+    const p = 0.5 * (this.a + this.b + this.c);
+    const result = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+
+    return Math.floor(result * 100) / 100;
+  }
+}
+
+export class Circle implements Figure {
+  constructor(
+    public color: Color,
+    public radius: number,
+    public shape: Shape,
+  ) {
+    this.color = color;
+    this.radius = radius;
+    this.shape = shape;
+  }
+
+  getArea(): number {
+    if (this.radius <= 0) {
+      throw new Error('your error message');
+    }
+
+    return Math.floor(Math.PI * this.radius ** 2 * 100) / 100;
+  }
+}
+
+export class Rectangle implements Figure {
+  constructor(
+    public color: Color,
+    public shape: Shape,
+    public width: number,
+    public height: number,
+  ) {
+    this.color = color;
+    this.shape = shape;
+    this.width = width;
+    this.height = height;
+  }
+
+  getArea(): number {
+    if (this.width <= 0 || this.height <= 0) {
+      throw new Error('your error message');
+    }
+
+    return Math.floor(this.width * this.height * 100) / 100;
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
